e5a07107184e22dd3650afdff0e931618c46cd05
Bejarano03 <marco.bejarano03@gmail.com>
2024-04-23 23:57:54 -0700

    created a profile maker app

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <><View style={styles.header}>
+      <Text>Title</Text>
+    </View><View style={styles.container}>
+        <Image source={{
+          uri: `https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGlmdjdtOWY0aHJwbndxNWVjMDZmeHltYW9hbXg0MDJ6Ymx5eGFobCZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/f8DhfShuYeMhGRES8x/200.webp`
+        }}
+          style={styles.img} />
+      </View></>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    flex: 1,
+    backgroundColor: '#eee',
+  },
+  img: {
+    height: '200px',
+  }
 });
